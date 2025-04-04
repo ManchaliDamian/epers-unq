74ff44a13d02516bc0b8c3b518a1bb653283a23c
Lourdes de la Cerda <lourdesdlc19@gmail.com>
2025-04-04 11:46:50 -0300

    primeras anotaciones en modelo espiritu

--- src/main/java/ar/edu/unq/epersgeist/modelo/Espiritu.java
+++ src/main/java/ar/edu/unq/epersgeist/modelo/Espiritu.java
@@ -1,23 +1,24 @@
 package ar.edu.unq.epersgeist.modelo;
 
 import ar.edu.unq.epersgeist.modelo.exception.NivelDeConexionException;
+import lombok.*;
+import jakarta.persistence.*;
 
 import java.io.Serializable;
 
-public class Espiritu implements Serializable {
+@Getter @Setter @NoArgsConstructor @ToString
 
+@Entity
+public class Espiritu implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String tipo;
     private Integer nivelDeConexion;
     private String nombre;
 
-    private static void validarNivelDeConexion(Integer nivelDeConexion) {
-        if (nivelDeConexion < 0 || nivelDeConexion > 100) {
-            throw new IllegalArgumentException("El nivel de conexión debe ser entre 0 y 100.");
-        }
-    }
 
-    public Espiritu(String tipo, Integer nivelDeConexion, String nombre) {
+    public Espiritu(@NonNull String tipo, @NonNull Integer nivelDeConexion, @NonNull String nombre) {
         validarNivelDeConexion(nivelDeConexion);
         this.tipo = tipo;
         this.nivelDeConexion = nivelDeConexion;
@@ -25,7 +26,7 @@ public class Espiritu implements Serializable {
     }
 
     // CONSULTAR POR ESTA SOLUCION
-    public Espiritu(Long id, String tipo, Integer nivelDeConexion, String nombre) {
+    public Espiritu(@NonNull Long id, @NonNull String tipo, @NonNull Integer nivelDeConexion, @NonNull String nombre) {
         validarNivelDeConexion(nivelDeConexion);
         this.id = id;
         this.tipo = tipo;
@@ -35,6 +36,12 @@ public class Espiritu implements Serializable {
 
     public void aumentarConexion(Medium medium) {
         nivelDeConexion = Math.min(nivelDeConexion + 10, 100);
+    }
+
+    private static void validarNivelDeConexion(Integer nivelDeConexion) {
+        if (nivelDeConexion < 0 || nivelDeConexion > 100) {
+            throw new IllegalArgumentException("El nivel de conexión debe ser entre 0 y 100.");
+        }
     }
 
     public Long getId() {
