1b427507262aaef72c5c71e45699da94cd9131e7
Damian Manchali <dami.manchali.eze@gmail.com>
2025-04-11 14:03:46 -0300

    testInvocar

--- src/test/java/ar/edu/unq/epersgeist/servicios/impl/MediumServiceTest.java
+++ src/test/java/ar/edu/unq/epersgeist/servicios/impl/MediumServiceTest.java
@@ -42,7 +42,7 @@ public class MediumServiceTest {
         ubicacionDAO = new HibernateUbicacionDAO();
         mediumDAO = new HibernateMediumDAO();
         espirituDAO = new HibernateEspirituDAO();
-
+        generadorMock = mock(GeneradorDeNumeros.class);
         serviceU = new UbicacionServiceImpl(ubicacionDAO);
         serviceM = new MediumServiceImpl(mediumDAO, espirituDAO);
         serviceE = new EspirituServiceImpl(espirituDAO, mediumDAO);
@@ -55,11 +55,31 @@ public class MediumServiceTest {
         medium1 = new Medium("Pablo", 100, 50, plata);
         medium2 = new Medium("Fidol", 100, 50, ubicacion);
         espiritu = new EspirituDemoniaco(80, "Jose", ubicacion, generadorMock);
-
+        angel = new EspirituAngelical(100, "kici", plata, generadorMock);
         serviceM.crear(medium1);
         serviceM.crear(medium2);
         serviceE.guardar(espiritu);
+        serviceE.guardar(angel);
     }
+//    @Test
+//    void testExorcizar(){
+//
+//
+//        when(generadorMock.entre(1, 100)).thenReturn(30); // ejemplo de número aleatorio
+//        medium2.conectarseAEspiritu(espiritu);
+//        medium1.conectarseAEspiritu(angel);
+//        serviceM.actualizar(medium2);
+//        serviceE.actualizar(espiritu);
+//        serviceM.actualizar(medium1);
+//        serviceE.actualizar(angel);
+//
+//        serviceM.exorcizar(medium1.getId(), medium2.getId());
+//        Medium recuperarM = serviceM.recuperar(medium1.getId());
+//        assertEquals(20, recuperarM.getEspiritus()
+//                    .stream()
+//                    .filter(e -> e.getId() == espiritu.getId())
+//                    .findFirst());
+//    }
     @Test
     void testInvocar() {
         Espiritu invocado = serviceM.invocar(medium1.getId(), espiritu.getId());
@@ -109,23 +129,7 @@ public class MediumServiceTest {
         verify(ang3, times(1)).descansar();
     }
 
-    @Test
-    void testExorcizar(){
-        medium2.conectarseAEspiritu(espiritu);
-        medium1.conectarseAEspiritu(angel);
 
-
-
-        medium1.exorcizarA(medium2);
-
-        serviceM.actualizar(medium2);
-        serviceE.actualizar(espiritu);
-        serviceM.actualizar(medium1);
-        serviceE.actualizar(angel);
-
-        assertEquals();
-
-    }
 
 
     @AfterEach
