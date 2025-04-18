a5a7e67190488014ea4ff06840770f19eda559a4
Hernán Moreyra <hernan_hello@hotmail.com>
2025-04-18 09:07:59 -0300

    query fix

--- src/main/java/ar/edu/unq/epersgeist/persistencia/dao/impl/HibernateEspirituDAO.java
+++ src/main/java/ar/edu/unq/epersgeist/persistencia/dao/impl/HibernateEspirituDAO.java
@@ -29,7 +29,7 @@ public class HibernateEspirituDAO extends HibernateDAO<Espiritu> implements Espi
     public List<Espiritu> recuperarDemoniacosPaginados(Direccion direccion, int pagina, int cantidadPorPagina){
         Session session = HibernateTransactionRunner.getCurrentSession();
         String orden = direccion == Direccion.ASCENDENTE ? "asc" : "desc";
-        String hql = "from Espiritu e where e.class = 'DEMONIACO' order by e.nivelDeConexion " + orden;
+        String hql = "from EspirituDemoniaco order by e.nivelDeConexion " + orden;
 
         Query<Espiritu> query = session.createQuery(hql, Espiritu.class);
         query.setFirstResult(pagina * cantidadPorPagina);
