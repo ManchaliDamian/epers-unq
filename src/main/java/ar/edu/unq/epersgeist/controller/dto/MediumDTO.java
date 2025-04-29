3aef9c0ba230243b69aa4137859820470aa2155f
FranciscoSRaffin <francisco.s.raffin@gmail.com>
2025-04-29 19:07:14 -0300

    initial commit

--- src/main/java/ar/edu/unq/epersgeist/controller/dto/MediumDTO.java
+++ src/main/java/ar/edu/unq/epersgeist/controller/dto/MediumDTO.java
@@ -1,4 +1,24 @@
 package ar.edu.unq.epersgeist.controller.dto;
+import ar.edu.unq.epersgeist.modelo.Medium;
+import ar.edu.unq.epersgeist.modelo.ubicacion.Ubicacion;
+import jakarta.validation.constraints.NotBlank;
 
-public record MediumDTO() {
+import java.util.List;
+
+public record MediumDTO(
+        Long id,
+        @NotBlank String nombre,
+        Ubicacion ubicacion,
+        Integer manaMax,
+        Integer mana,
+        List<String> espiritus
+        ) {
+
+    public static MediumDTO desdeModelo(Medium medium){
+
+    };
+
+    public static MediumDTO aModelo(Medium medium){
+
+    };
 }
