4729a3b25589083ae92e63cabdcf1d81f804631e
axlg98 <axlgarcia080498@gmail.com>
2025-04-29 23:28:34 -0300

    avanzando test estadisticaService.

--- src/main/java/ar/edu/unq/epersgeist/controller/dto/EspirituDTO.java
+++ src/main/java/ar/edu/unq/epersgeist/controller/dto/EspirituDTO.java
@@ -4,19 +4,19 @@ import ar.edu.unq.epersgeist.modelo.Espiritu;
 
 //Ver luego que agregar, falta agregar UbicacionDTO 
 //A lo último, terminar de agregar el tipo de espiritu
-public record EspirituDTO(Long id, String nombre,Integer nivelDeConexion,Long mediumConectadoId) {
+public record EspirituDTO(Long id, String nombre,Integer nivelDeConexion,Long mediumConectadoId,UbicacionDTO ubicacion) {
     public static EspirituDTO desdeModelo(Espiritu espiritu) {
         return new EspirituDTO(
                 espiritu.getId(),
                 espiritu.getNombre(),
                 espiritu.getNivelDeConexion(),
-                espiritu.getMediumConectado() != null ? espiritu.getMediumConectado().getId() : null
+                espiritu.getMediumConectado() != null ? espiritu.getMediumConectado().getId() : null,
+                UbicacionDTO.desdeModelo(espiritu.getUbicacion())
         );
     }
 
-//Preguntar si se puede usar el switch case. Ya que, se necesita saber si es angelical o demoniaco
 //Luego lo termino cuando estén hechos los de UbicaciónDTO, pero es algo así
-// UbicacionDTO ubicacion en el record
+//En un rato lo arreglo con switch case:
 //    public Espiritu aModelo(){
 //        Espiritu espiritu = new Espiritu(this.nombre, ubicacion.aModelo());
 //        espiritu.setId(this.id);
