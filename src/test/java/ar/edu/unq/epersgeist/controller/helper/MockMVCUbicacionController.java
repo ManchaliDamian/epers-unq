7fbad0c0e167f1144f0f224cb5df8dd98e0f71e9
Hernán Moreyra <hernan_hello@hotmail.com>
2025-05-03 00:13:28 -0300

    borrado import

--- src/test/java/ar/edu/unq/epersgeist/controller/helper/MockMVCUbicacionController.java
+++ src/test/java/ar/edu/unq/epersgeist/controller/helper/MockMVCUbicacionController.java
@@ -11,7 +11,6 @@ import jakarta.servlet.ServletException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
