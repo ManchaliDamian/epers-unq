a71a7a4aed215cf88657c47b96ad82291ae712f9
Hernán Moreyra <hernan_hello@hotmail.com>
2025-05-18 00:24:29 -0300

    fix mensaje error

--- src/main/java/ar/edu/unq/epersgeist/modelo/exception/UbicacionNoEliminableException.java
+++ src/main/java/ar/edu/unq/epersgeist/modelo/exception/UbicacionNoEliminableException.java
@@ -3,7 +3,7 @@ package ar.edu.unq.epersgeist.modelo.exception;
 public class UbicacionNoEliminableException extends RecursoNoEliminableException {
     public UbicacionNoEliminableException(Long id) {
         super(
-                "No es posible eliminar la Ubicacion con ID:" + id + " porque posee al menos un Espiritu o un Medium"
+                "No es posible eliminar la Ubicacion con ID: " + id + " porque posee al menos un Espiritu o un Medium"
         );
     }
 }
