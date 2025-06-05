480e25852a3b4f9f318d18f9740098231c04334a
Lourdes de la Cerda <lourdesdlc19@gmail.com>
2025-06-05 00:50:25 -0300

    elimino desdeModelo de UpdateDTO, nunca se usa

--- src/main/java/ar/edu/unq/epersgeist/controller/dto/UpdateMediumDTO.java
+++ src/main/java/ar/edu/unq/epersgeist/controller/dto/UpdateMediumDTO.java
@@ -7,12 +7,6 @@ public record UpdateMediumDTO(
         @NotBlank String nombre,
         Integer manaMax
 ) {
-    public static UpdateMediumDTO desdeModelo(Medium medium) {
-        return new UpdateMediumDTO(
-                medium.getNombre(),
-                medium.getManaMax()
-        );
-    }
     public void actualizarModelo(Medium medium) {
         medium.setNombre(this.nombre());
         medium.setManaMax(this.manaMax());
--- src/main/java/ar/edu/unq/epersgeist/controller/dto/UpdateUbicacionDTO.java
+++ src/main/java/ar/edu/unq/epersgeist/controller/dto/UpdateUbicacionDTO.java
@@ -6,13 +6,8 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public record UpdateUbicacionDTO(@NotBlank String nombre, @NotNull @Min(1) @Max(100) Integer flujoDeEnergia) {
-    public static UpdateUbicacionDTO desdeModelo(Ubicacion ubicacion) {
-        return new UpdateUbicacionDTO(
-                ubicacion.getNombre(),
-                ubicacion.getFlujoDeEnergia()
-        );
-    }
+public record UpdateUbicacionDTO(@NotBlank String nombre,
+                                 @NotNull @Min(1) @Max(100) Integer flujoDeEnergia) {
 
     public void actualizarModelo(Ubicacion ubicacion) {
         ubicacion.setNombre(this.nombre());
