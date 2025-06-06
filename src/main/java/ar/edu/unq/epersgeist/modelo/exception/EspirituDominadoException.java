df7b2c5775c019cc388afb230c651fc8256cc400
Lourdes de la Cerda <lourdesdlc19@gmail.com>
2025-06-05 22:13:20 -0300

    espiritu muy lejano exception + espiritu dominado exception

---
+++ src/main/java/ar/edu/unq/epersgeist/modelo/exception/EspirituDominadoException.java
@@ -0,0 +1,7 @@
+package ar.edu.unq.epersgeist.modelo.exception;
+
+public class EspirituDominadoException extends RuntimeException {
+  public EspirituDominadoException(String message) {
+    super(message);
+  }
+}
---
+++ src/main/java/ar/edu/unq/epersgeist/modelo/exception/EspirituMuyLejanoException.java
@@ -0,0 +1,7 @@
+package ar.edu.unq.epersgeist.modelo.exception;
+
+public class EspirituMuyLejanoException extends RuntimeException {
+    public EspirituMuyLejanoException(String message) {
+        super(message);
+    }
+}
