2f0d5205422d900b341ebdbabeafbc5f887107cc
ManchaliDamian <dami.manchali.eze@gmail.com>
2025-06-13 10:05:12 -0300

    handler de error agregado para conflict

--- src/main/java/ar/edu/unq/epersgeist/controller/ExceptionHandlerREST.java
+++ src/main/java/ar/edu/unq/epersgeist/controller/ExceptionHandlerREST.java
@@ -1,6 +1,7 @@
 package ar.edu.unq.epersgeist.controller;
 
 import ar.edu.unq.epersgeist.controller.dto.ErrorDTO;
+import ar.edu.unq.epersgeist.exception.DistanciaNoCercanaException;
 import ar.edu.unq.epersgeist.exception.EspirituNoDominableException;
 import ar.edu.unq.epersgeist.exception.NombreDeUbicacionRepetidoException;
 import ar.edu.unq.epersgeist.exception.RecursoNoEliminableException;
@@ -16,6 +17,8 @@ public class ExceptionHandlerREST {
     @ExceptionHandler({
             NombreDeUbicacionRepetidoException.class,
             RecursoNoEliminableException.class,
+            EspirituNoDominableException.class,
+            DistanciaNoCercanaException.class
     })
     @ResponseStatus(HttpStatus.CONFLICT)
     public ErrorDTO handleConflict(RuntimeException ex) {
@@ -32,11 +35,6 @@ public class ExceptionHandlerREST {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorDTO handleIllegalArgumentException(IllegalArgumentException ex) {
         return new ErrorDTO(ex.getMessage(), HttpStatus.BAD_REQUEST.value());
-    }
-    @ExceptionHandler(EspirituNoDominableException.class)
-    @ResponseStatus(HttpStatus.CONFLICT)
-    public ErrorDTO handleEspirituNoDominableException(EspirituNoDominableException ex) {
-        return new ErrorDTO(ex.getMessage(), HttpStatus.CONFLICT.value());
     }
 }
 
