7dc84a696d9a02a11390ce8180ea1bdd9e89bd53
Hernán Moreyra <hernan_hello@hotmail.com>
2025-06-25 18:27:26 -0300

    config inicial

--- src/main/java/ar/edu/unq/epersgeist/configuration/AppConfiguration.java
+++ src/main/java/ar/edu/unq/epersgeist/configuration/AppConfiguration.java
@@ -1,6 +1,33 @@
 package ar.edu.unq.epersgeist.configuration;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
+import com.google.cloud.firestore.Firestore;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
-public class AppConfiguration { }
+public class AppConfiguration {
+
+    @Bean
+    public FirebaseApp initializeFirebase() throws IOException {
+        InputStream serviceAccount = new ClassPathResource("epers-key.json").getInputStream();
+
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        return FirebaseApp.initializeApp(options);
+    }
+
+    @Bean
+    public Firestore firestore(FirebaseApp firebaseApp) {
+        return FirestoreClient.getFirestore(firebaseApp);
+    }
+}
